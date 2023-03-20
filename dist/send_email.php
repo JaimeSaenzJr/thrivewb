66d811a78edd0e18e809bdbcab22399cb533ca9d
Jaime Saenz <53834960+JaimeSaenz@users.noreply.github.com>
2023-03-19 21:30:55 -0700

    Updated php

--- dist/send_email.php
+++ dist/send_email.php
@@ -6,35 +6,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = $_POST["name"];
   $organization = $_POST["organization"];
   $from = $_POST["email"];
-  $attachment1 = $_FILES["attachment1"];
+  $attachment1 = $_FILES["attachments"];
   $attachment2 = $_FILES["attachment2"];
 
+  $boundary = uniqid('np');
   $headers = "From: $from\r\n";
   $headers .= "Reply-To: $from\r\n";
-  $headers .= "Content-Type: multipart/mixed; boundary=boundary\n";
+  $headers .= "Content-Type: multipart/mixed; boundary=$boundary\r\n";
 
   $message = "Name: $name\n";
   $message .= "Organization: $organization\n\n";
 
-  $content = chunk_split(base64_encode(file_get_contents($attachment1["tmp_name"])));
-  $filename = $attachment1["name"];
-  $message .= "--boundary\n";
-  $message .= "Content-Type: {\"application/octet-stream\"};\n";
-  $message .= " name=\"$filename\"\n";
-  $message .= "Content-Disposition: attachment;\n";
-  $message .= " filename=\"$filename\"\n";
-  $message .= "Content-Transfer-Encoding: base64\n\n";
-  $message .= "$content\n";
+  if ($attachment1["error"] == UPLOAD_ERR_OK) {
+    $content = chunk_split(base64_encode(file_get_contents($attachment1["tmp_name"])));
+    $filename = $attachment1["name"];
+    $message .= "--$boundary\n";
+    $message .= "Content-Type: {\"application/octet-stream\"};\n";
+    $message .= " name=\"$filename\"\n";
+    $message .= "Content-Disposition: attachment;\n";
+    $message .= " filename=\"$filename\"\n";
+    $message .= "Content-Transfer-Encoding: base64\n\n";
+    $message .= "$content\n";
+  }
 
-  $content2 = chunk_split(base64_encode(file_get_contents($attachment2["tmp_name"])));
-  $filename2 = $attachment2["name"];
-  $message .= "--boundary\n";
-  $message .= "Content-Type: {\"application/octet-stream\"};\n";
-  $message .= " name=\"$filename2\"\n";
-  $message .= "Content-Disposition: attachment;\n";
-  $message .= " filename=\"$filename2\"\n";
-  $message .= "Content-Transfer-Encoding: base64\n\n";
-  $message .= "$content2\n";
+  if ($attachment2["error"] == UPLOAD_ERR_OK) {
+    $content2 = chunk_split(base64_encode(file_get_contents($attachment2["tmp_name"])));
+    $filename2 = $attachment2["name"];
+    $message .= "--$boundary\n";
+    $message .= "Content-Type: {\"application/octet-stream\"};\n";
+    $message .= " name=\"$filename2\"\n";
+    $message .= "Content-Disposition: attachment;\n";
+    $message .= " filename=\"$filename2\"\n";
+    $message .= "Content-Transfer-Encoding: base64\n\n";
+    $message .= "$content2\n";
+  }
 
   if (mail($to, $subject, $message, $headers)) {
     echo "Thank you for your submission.";
